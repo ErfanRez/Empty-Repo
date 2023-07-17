80b7fa5542fcd225ffc80aeded636415152c5216
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-07-17 22:30:43 +0300

    App file has been updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ import Confetti from "react-confetti"
 
 export default function App ()
 {
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
 
+
+    React.useEffect(() => 
+    {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue)
+        {
+            setTenzies(true)
+        }
+    },[dice])
+
+
+    function generateNewDie ()
+    {
+        return { 
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
 
 
     return (
