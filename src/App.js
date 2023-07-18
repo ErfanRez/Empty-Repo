adb773926028314704c9e0ac22c9be0f8c53490d
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-07-18 19:51:36 +0300

    App.js file has been updated

--- src/App.js
+++ src/App.js
@@ -31,9 +31,67 @@ export default function App ()
     }
 
 
+    function allNewDice()
+    {
+        const newDice = []
+        for (let i = 0 ; i < 10 ; i++)
+        {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+
+
+    
+    function rollDice()
+    {
+        if (!tenzies)
+        {
+            setDice(oldDice => oldDice.map(die => 
+                {
+                    return (
+    
+                        die.isHeld ?
+                        die : generateNewDie()
+    
+                    ) 
+                }))
+        }
+        else {
+            setTenzies(false)
+            setDice(allNewDice())
+        }
+        
+    }
+
+    function holdDice(id)
+    {
+        setDice(oldDice => oldDice.map(die => 
+            {
+                return (
+
+                    die.id === id ? 
+                    {...die, isHeld : !die.isHeld} :
+                    die
+
+                )             
+            }))
+    }
+
+
+    const diceElements = dice.map (die => <Die key = {die.id} value = {die.value} isHeld = {die.isHeld} holdDice = {() => holdDice(die.id)} />)
+
+
     return (
     <main>
-        
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze
+        it at its current value between rolls.</p>
+        <div className="dice-container">
+            {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
+        {tenzies && <Confetti/>}
     </main>
     )
 }
